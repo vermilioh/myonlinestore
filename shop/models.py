4170169a76270fee52118e3dbbca1fea5b8a373a
vermilioh <vermilion296@gmail.com>
2023-06-08 12:36:57 +0400

    fix images

--- shop/models.py
+++ shop/models.py
@@ -33,7 +33,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='media/products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
